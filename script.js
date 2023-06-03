539427fbde9bf83d65a423b300c7a10abf335797
aski361 <karpowiczaskaniusz@outlook.com>
2023-06-03 08:35:20 +0200

    make calculator work

--- script.js
+++ script.js
@@ -15,53 +15,61 @@ const divide = function(a, b) {
 }
     
 let firstNumber = '';
-let operator = '';
 let secondNumber = '';
-let upperDisplay = '';
-let downDisplay = '';
-let result = '';
+let currentOperator = null;
+let shouldScreenReset = false;
+
 
     
 const operate = function(operator, firstNumber, secondNumber) {
     return operator == ' + ' ? add(Number(firstNumber), Number(secondNumber)):
-    operator == '-' ? substract(firstNumber, secondNumber) :
-    operator == '*' ? multiply(firstNumber, secondNumber) :
-    operator == '/' ? divide(firstNumber, secondNumber) : 'Podano zły operator'
+    operator == ' - ' ? substract(firstNumber, secondNumber) :
+    operator == ' * ' ? multiply(firstNumber, secondNumber) :
+    operator == ' / ' ? divide(firstNumber, secondNumber) : 'Podano zły operator'
 }
 
 const numberButtons = document.querySelectorAll('.number');
 const operation = document.querySelector('.operation');
 const operand = document.querySelectorAll('.operand');
-const resultDiv = document.querySelector('.result')
+const currentOperation = document.querySelector('.result')
 const clear = document.querySelector('#clear');
+const equal = document.querySelector('#equal');
 
-clear.addEventListener('click', () => {
-    resultDiv.textContent = '0';
-    operation.textContent = '';
-    firstNumber = '';
-    operator = '';
-    secondNumber = '';
-    upperDisplay = '';
-    downDisplay = '';
-    result = '';
-})
 
 numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        
-    })
+    button.addEventListener('click', () => appendNumber(button.innerHTML))
 })
+
+function appendNumber(number) {
+    if(currentOperation.textContent === '0' || shouldScreenReset) resetScreen()
+    currentOperation.textContent += number;
+}
+
+function resetScreen() {
+    currentOperation.textContent = ''
+    shouldScreenReset = false;
+}
+
 
 operand.forEach(button => {
     button.addEventListener('click', () => {
-        
+        setOperation(button.innerHTML);
     })
 })
 
+function setOperation(operator) {
+    if(currentOperation !== null) evaluate();
+    firstNumber = currentOperation.textContent;
+    currentOperator = operator;
+    operation.textContent = `${firstNumber} ${operator}`;
+    shouldScreenReset = true;
+}
 
-
-
-
-
-
+function evaluate() {
+    if (currentOperator === null || shouldScreenReset) return
+    secondNumber = currentOperation.innerHTML
+    currentOperation.textContent = operate(currentOperator,firstNumber,secondNumber);
+    operation.textContent = `${firstNumber} ${currentOperation} ${secondNumber} =`
+    currentOperator = null;
+}
 
